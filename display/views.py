462536c6d582ac6178944cfcb5f11089ce0fdb51
ashrfsadik <ashrfsadik73@gmail.com>
2024-04-04 11:48:03 +0200

    save the register

--- display/views.py
+++ display/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from urllib.parse import unquote
+from .models import Display
 
 
 # Create your views here.
@@ -18,3 +19,14 @@ def display_web(request, url):
     
     # مرر الـ embed_url إلى القالب
     return render(request, 'display/webviewA.html', {'embed_url': full_url})
+def submit_operation(request):
+    if request.method == 'POST':
+        choose_value = request.POST.get('CHOOSE')  # احصل على القيمة المختارة
+        display_id = request.POST.get('display_id')  # احصل على معرف العرض
+        
+        # قم بتحديث العرض المرتبط بالمعرف مع القيمة المختارة
+        display = Display.objects.get(pk=display_id)
+        display.choosenum = choose_value
+        display.save()
+
+    return redirect('display/videoA.html')  # قم بتوجيه المستخدم مرة أخرى إلى صفحة العرض بعد الانتهاء
