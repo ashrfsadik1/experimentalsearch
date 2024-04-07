1476df11cd33ed4b54e94cb43c39d6c06ec4ce38
ashrfsadik <ashrfsadik73@gmail.com>
2024-04-07 10:45:40 +0200

    make a decent change to display models

--- display/models.py
+++ display/models.py
@@ -5,22 +5,19 @@ from email.policy import default
 from unicodedata import category
 from django.db import models
 from datetime import datetime
-from .models import Display
+
 
 
 class Display(models.Model) :
     url=models.URLField(unique=True)
     text = models.CharField(max_length=150) 
     
-   
-    
-    
-    class  Display_Data(models.Model) :
+class  Display_Data(models.Model) :
          displays = models.ManyToManyField(Display)  
          user= models.ManyToManyField(User)
          choosenum=models.IntegerField()
          puplish_date =models.DateTimeField(default=datetime.now) 
-    class Meta:
+class Meta:
         ordering =['-puplish_date']
 
     
