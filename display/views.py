48567cdbf36b9af65418279ba3805f9bc5ca4f86
ashrfsadik <ashrfsadik73@gmail.com>
2024-04-09 17:22:01 +0200

    good display

--- display/views.py
+++ display/views.py
@@ -9,22 +9,14 @@ import requests
 def display_video(request, url):
     # تشكيل الـ URL الكامل لإطار الفيديو على YouTube
     embed_url = f"https://www.youtube.com/embed/{url}"
+    full_url = f"https://www.youtube.com/watch?v={url}"
+    soup = BeautifulSoup(requests.get(full_url).content, "html.parser")
+    title = soup.title.text
+    
 
-    # استخراج عنوان الفيديو من URL
-    try:
-        with YoutubeDL({'verbose': True}) as ydl:
-            meta = ydl.extract_info(url, download=False)
-            title = meta.get('title')
-            print ('hello')
-            print(title)
-            print('hello')
-    except YoutubeDLException as e:
-        # Handle the exception, you can display a user-friendly message
-        return render(request, 'display/videoA.html', {'error': str(e)})
-       
+        # مرر الـ embed_url وعنوان الفيديو إلى القالب
+    return render(request, 'display/videoA.html', {'embed_url': embed_url , 'title': title})
 
-    # مرر الـ embed_url وعنوان الفيديو إلى القالب
-    return render(request, 'display/videoA.html', {'embed_url': embed_url, 'title': title})
 def display_web(request, url):
     # تشكيل الـ URL الكامل لإطار الفيديو على YouTube
     full_url = unquote(url)
@@ -32,11 +24,6 @@ def display_web(request, url):
     # استخراج عنوان الموقع من URL
     soup = BeautifulSoup(requests.get(full_url).content, "html.parser")
     title = soup.title.text
-    print("hello")
-    print (title)
-    print ('hello')
-
-    # مرر الـ embed_url وعنوان الموقع إلى القالب
     return render(request, 'display/webviewA.html', {'embed_url': full_url, 'title': title})
 
 def submit_operation(request):
