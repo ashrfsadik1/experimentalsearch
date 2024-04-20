d07df72d9db5620097dac174a160cb4e6c99d5ea
ashrfsadik <ashrfsadik73@gmail.com>
2024-04-20 18:30:14 +0200

    database soluthon

--- display/models.py
+++ display/models.py
@@ -15,7 +15,7 @@ class Display(models.Model) :
     
 class  Display_Data(models.Model) :
          displays = models.ManyToManyField(Display,related_name='display_data')  
-         users= models.ManyToManyField(UserProfile,related_name='display_profile')
+         users= models.ForeignKey(UserProfile,on_delete=models.CASCADE,default="1")
          choosenum=models.IntegerField()
          puplish_date =models.DateTimeField(default=datetime.now) 
 class Meta:
