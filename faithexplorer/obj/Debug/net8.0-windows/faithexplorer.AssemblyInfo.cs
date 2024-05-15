21330fc89af0baa77886a8b0e454855c2d0046fb
ashrfsadik <ashrfsadik73@gmail.com>
2024-05-15 20:15:57 +0300

    begin in translate to engilsh

--- faithexplorer/obj/Debug/net8.0-windows/faithexplorer.AssemblyInfo.cs
+++ faithexplorer/obj/Debug/net8.0-windows/faithexplorer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("faithexplorer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89a4a09317e777f116f406544593a66c19b557f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8452f6402f8ddab8233a0b3f060a4765e582a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("faithexplorer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("faithexplorer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
