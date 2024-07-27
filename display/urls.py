a497d089c3c6c34dbc72fe05ec8ffd2b960cec95
ashrfsadik <ashrfsadik73@gmail.com>
2024-07-27 14:21:38 +0300

    solve problem of urls

--- display/urls.py
+++ display/urls.py
@@ -8,8 +8,13 @@ from .views import displaymydelayoperations
 
 from . import views
 urlpatterns = [
-    path('display/<path:url>/<str:searchtxt>', display_web, name='display_web'),
-     path('display/display_video/<path:url>/<str:searchtxt>', views.display_video, name='display_video'),
+    #path('display/<path:url>/<str:searchtxt>', display_web, name='display_web'),
+
+
+    re_path(r'^display/display/(?P<url>.+)/(?P<searchtxt>.*)$', views.display_web, name='display_web'),
+     re_path(r'^display/display_video(?P<url>.+)/(?P<searchtxt>.*)$', views.display_video, name='display_video'),
+  
+     #path('display/display_video/<path:url>/<str:searchtxt>', views.display_video, name='display_video'),
     #re_path(r'^display_web/(?P<url>.+)$',views.display_web, name='display_web'),
     path('submit_operation/', submit_operation, name='submit_operation'),
     path('displaymyoperations/', displaymyoperations, name='displaymyoperations'),
